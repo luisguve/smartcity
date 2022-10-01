230cebf81e7d6b72646e69395f3f3f3c1843e293
Luis G. Villegas <luisguveal@gmail.com>
2022-09-30 22:49:51 -0400

    Switching to vue

---
+++ frontend/vue/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="increment()">increment {{count}}!</button>
+</template>
+
+<script>
+  import { mapState, mapActions } from "pinia";
+  import { useMainStore } from "../store";
+  export default {
+    data() {
+      return {};
+    },
+    computed: {
+      ...mapState(useMainStore, ["count"])
+    },
+    methods: {
+      ...mapActions(useMainStore, ["increment"])
+    }
+  };
+</script>
